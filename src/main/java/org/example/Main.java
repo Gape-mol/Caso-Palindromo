521faeca1b3e25d03360370cebf20f597a2b9c2a
Gape-mol <g.perez11@ufromail.cl>
2024-09-02 16:26:51 -0400

    Version en java de la funcion de js

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (esPalindromo("")){
+            System.out.println("Es palindromo");
+        }
+        else{
+            System.out.println("No es palindromo");
+        }
+    }
+    public static boolean esPalindromo(String cadena){
+        String resultado = "";
+        for (int i = cadena.length() - 1; i >= 0; i--) {
+            resultado += cadena.charAt(i);
+        }
+        System.out.println(resultado);
+        System.out.println(cadena);
+        return resultado.equals(cadena);
     }
 }
