578b124816cea986e98e78ac2f2501efcce7e409
BenjaG123 <b.garces01@ufromail.cl>
2024-09-08 16:31:02 -0300

    Se actualiza el codigo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello  world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese una palabra: ");
+        String palabra = scanner.nextLine();
+        if (esPalindromo(palabra)) {
+            System.out.println("La palabra "+palabra+" es palindromo");
+        } else {
+            System.out.println("La palabra "+palabra+" no es palindromo");
+        }
+    }
+    public static boolean esPalindromo(String palabra) {
+        return palabra.equals(new StringBuilder(palabra).reverse().toString());
     }
 }
