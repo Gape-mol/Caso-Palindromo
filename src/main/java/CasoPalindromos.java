f22ed20aa4777a69eb015b1712f7cc83a87f85bc
Francisco <f.lizama05@ufromail.cl>
2024-09-08 18:27:47 -0300

    Borrados print's innecesarios

--- src/main/java/CasoPalindromos.java
+++ src/main/java/CasoPalindromos.java
@@ -2,11 +2,10 @@ public class CasoPalindromos {
     public static boolean esPalindromo(String cadena){
         String resultado = "";
         String cadenaMinusculasSinEspacios = cadena.toLowerCase().replaceAll(" ", "");
-        System.out.println(cadenaMinusculasSinEspacios);
         for (int letra = cadenaMinusculasSinEspacios.length()-1; letra >=0 ; letra--) {
             resultado += cadenaMinusculasSinEspacios.charAt(letra);
         }
-        System.out.println(resultado);
+
         return resultado.equals(cadenaMinusculasSinEspacios);
     }
 }
